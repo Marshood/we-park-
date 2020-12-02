46aa3466bdf3e673f95f308de1895d7cca6e1c6f
Nivin <nivin.abbas1@gmail.com>
2020-12-03 00:25:18 +0200

    added component login with logo

---
+++ we-park/src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from "../../assets/logo_tp.png"
+const Login = (props) => {
+    return (    
+                <div>
+                    <img src={logo}/>
+
+                 </div>
+    
+            );
+}
+ 
+export default Login;
