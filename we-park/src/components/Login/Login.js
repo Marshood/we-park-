10114d4b1f110087fd7f1c8ee93ab030dbb6ea2c
Nivin <nivin.abbas1@gmail.com>
2020-12-03 02:33:26 +0200

    fix background screen resizing

--- we-park/src/components/Login/Login.js
+++ we-park/src/components/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from "../../assets/logo_tp.png";
-// import back from "../../assets/129177850_503303760560488_7379409967702803208_n.png"
-// import "./Login.css"
+import "./Login.css"
 const Login = (props) => {
     return (    
         <div>
