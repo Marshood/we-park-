2f6eda5c6e971227cd2b1b417d9f83bc46ccc0ed
Omri Zaher <omriza5@gmail.com>
2020-12-03 13:47:57 +0200

    add: logic to login form

--- we-park/src/components/Login/Login.js
+++ we-park/src/components/Login/Login.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo_tp.png";
 import "./Login.css";
 const Login = (props) => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    setUser({ ...user, [name]: e.currentTarget.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //call the server (post method)
+  };
+
   return (
     <div className="container">
       <img src={logo} className="logoIcon" />
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="inputsBox">
-          <input type="text" placeholder="User Name" className="input" />
-          <input type="password" placeholder="Password" className="input" />
+          <input
+            type="text"
+            placeholder="User Name"
+            name="username"
+            className="input"
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="input"
+            onChange={handleInputChange}
+          />
         </div>
 
         <button className="button">Login</button>
--- we-park/src/components/Profile/Profile.js
+++ we-park/src/components/Profile/Profile.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import "./Profile.css"
-import user from "../../assets/user(1).png"
-import edit from "../../assets/edit.png"
+import React from "react";
+import "./Profile.css";
+import user from "../../assets/user(1).png";
+import edit from "../../assets/edit.png";
 
 const Profile = (props) => {
-    return (
-        
-                 
-                       
-                   <div> 
-     <form >    
-         
-   
-<label className="imageFile" >
-   <img src={user}/>
-<input type="file" className="image"/>
-</label> 
-<button className="imgEdit" ><img src={edit} style={{width:32,height:32}}/></button>
-        <div className="textInfos" >  Guest Parker  </div>
+  return (
+    <div>
+      <form>
+        <label className="imageFile">
+          <img src={user} />
+          <input type="file" className="image" />
+        </label>
+        <button className="imgEdit">
+          <img src={edit} style={{ width: 32, height: 32 }} />
+        </button>
+        <div className="textInfos"> Guest Parker </div>
         <div className="textInfos">Test</div>
         <div className="textInfos">04-6166182</div>
         <div className="textInfos">test@test.com</div>
         <div className="textInfos">99-999-99</div>
+      </form>
+    </div>
+  );
+};
 
-                  </form>  
-                  </div>   
-
-    
-    );
-}
- 
 export default Profile;
