f6fecc9cb1ba9d995a2d9d69d287b4b85be6051d
Nivin <nivin.abbas1@gmail.com>
2020-12-04 19:12:23 +0200

    fix timer countdown

--- we-park/src/components/Timer/Timer.js
+++ we-park/src/components/Timer/Timer.js
@@ -1,20 +1,28 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import useCountDown from "react-countdown-hook";
+import React from "react";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
+
+// Random component
+const Completionist = () => <span>You Time is OUT</span>;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a complete state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span>
+       {minutes}:{seconds}
+      </span>
+    );
+  }
+};
+
 
 const Timer = (props) => {
-    const [timeLeft, actions] = useCountDown(150000, 1000);
-    const buttonStyle = { marginRight: "10px" };
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1 id="time-left">{(timeLeft / 1000)}</h1>
-        <button id="start" style={buttonStyle} onClick={() => actions.start()}>
-          Reserve
-        </button>
-  
-     
-      </div>
-    ); 
+  return ( <Countdown date={Date.now() + 900000} renderer={renderer} /> );
 }
  
 export default Timer;
