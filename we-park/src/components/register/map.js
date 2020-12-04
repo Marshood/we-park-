4daa73e2b82fac204cd9c0661c594689cc96bccc
Omri Zaher <omriza5@gmail.com>
2020-12-04 13:23:21 +0200

    add: styled popup component

--- we-park/src/components/register/map.js
+++ we-park/src/components/register/map.js
@@ -101,7 +101,6 @@
 //     lng: 34.989571
 // }
 
-
 // const Map = (props) => {
 //     const [map, setMap] = React.useState(null)
 
@@ -178,209 +177,183 @@
 //
 //
 //
-import 'mapbox-gl/dist/mapbox-gl.css'
-import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
-import React, { useState, useRef, useCallback } from 'react'
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { useState, useRef, useCallback } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import * as parkDate from '../register/data/skateboard-parks.json';
-import './map.css'
-import MapGL, { NavigationControl, GeolocateControl } from 'react-map-gl'
-import Geocoder from 'react-map-gl-geocoder'
+import * as parkDate from "../register/data/skateboard-parks.json";
+import "./map.css";
+import MapGL, { NavigationControl, GeolocateControl } from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
 //00000000000000000000000000000000000000000000
-import logo from "../../assets/logo_tp.png"
-import ParkingPopup from '../ParkingPopup/index'
-import { fromJS } from 'immutable';
+import logo from "../../assets/logo_tp.png";
+import ParkingPopup from "../ParkingPopup/index";
+import { fromJS } from "immutable";
 const mapStyle = fromJS({
-    //  version: 11,
-    sources: {
-        points: {
-            type: 'geojson',
-            data: {
-                type: 'FeatureCollection',
-                features: [
-                    { type: 'Feature', geometry: { type: 'Point', coordinates: [-122.45, 37.78] } }
-                    , { type: 'Feature', geometry: { type: 'Point', coordinates: [122.45, 37.78] } }
-                    , { type: 'Feature', geometry: { type: 'Point', coordinates: [122.45, +37.78] } }
-
-                ]
-            }
-        }
+  //  version: 11,
+  sources: {
+    points: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: { type: "Point", coordinates: [-122.45, 37.78] },
+          },
+          {
+            type: "Feature",
+            geometry: { type: "Point", coordinates: [122.45, 37.78] },
+          },
+          {
+            type: "Feature",
+            geometry: { type: "Point", coordinates: [122.45, +37.78] },
+          },
+        ],
+      },
     },
-    layers: [
-        {
-            id: 'my-layer',
-            type: 'circle',
-            source: 'points',
-            paint: {
-                'circle-color': '#f00',
-                'circle-radius': 4
-            }
-        }
-    ]
+  },
+  layers: [
+    {
+      id: "my-layer",
+      type: "circle",
+      source: "points",
+      paint: {
+        "circle-color": "#f00",
+        "circle-radius": 4,
+      },
+    },
+  ],
 });
 
-
-
 // Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
-const MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyc2hvb2RheW91YiIsImEiOiJja2k4MG81d2QwMTcxMnJvNTNrOWZwbzBmIn0.nI-lbYBSz7xNamt-QPx4mQ'
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoibWFyc2hvb2RheW91YiIsImEiOiJja2k4MG81d2QwMTcxMnJvNTNrOWZwbzBmIn0.nI-lbYBSz7xNamt-QPx4mQ";
 
 const Example = () => {
-    const [selectedPark, setSelectedPark] = useState(null);
-    const [selectedParkName, setSelectedParkName] = useState(null);
+  const [selectedPark, setSelectedPark] = useState(null);
+  const [selectedParkName, setSelectedParkName] = useState(null);
 
-    const [viewport, setViewport] = useState({
-        latitude: 32.9374355,// 32.794044,
-        longitude: 35.2697695,//34.989571,
-        zoom: 50,
-        // style:'mapbox://styles/mapbox/streets-v11'
+  const [viewport, setViewport] = useState({
+    latitude: 32.9374355, // 32.794044,
+    longitude: 35.2697695, //34.989571,
+    zoom: 10,
+    // style:'mapbox://styles/mapbox/streets-v11'
+  });
+  const styleUrl = "mapbox://styles/mapbox/outdoors-v11"; //"mapbox://styles/marshoodayoub/cki82mo14b4at19quioi8lqdz";
 
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback((newViewport) => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return handleViewportChange({
+      ...newViewport,
+      ...geocoderDefaultOverrides,
     });
-    const styleUrl = 'mapbox://styles/mapbox/outdoors-v11'//"mapbox://styles/marshoodayoub/cki82mo14b4at19quioi8lqdz";
+  }, []);
 
-    const mapRef = useRef();
-    const handleViewportChange = useCallback(
-        (newViewport) => setViewport(newViewport),
-        []
-    );
+  return (
+    <div className="map_continaer">
+      {navigator.geolocation.getCurrentPosition(function (position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+      })}
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        points
+        onClick={(e) => {
+          console.log(e);
+        }}
+      >
+        {parkDate.features.map((park) => (
+          <Marker
+            key={park.properties.PARK_ID}
+            latitude={park.geometry.coordinates[1]}
+            longitude={park.geometry.coordinates[0]}
+          >
+            <img
+              className="locationIcon"
+              src="https://www.flaticon.com/svg/vstatic/svg/684/684908.svg?token=exp=1607078169~hmac=d6c420c4eca04e97b5740cd3cf9dfd25"
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedPark(park);
+              }}
+            />
+          </Marker>
+        ))}
 
-    // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
-    const handleGeocoderViewportChange = useCallback(
-        (newViewport) => {
-            const geocoderDefaultOverrides = { transitionDuration: 1000 };
+        {selectedPark
+          ? //  alert(selectedPark.properties.NAME),
+            (setTimeout(function () {
+              setSelectedPark(null);
+            }, 200000),
+            (
+              <Popup
+                latitude={selectedPark.geometry.coordinates[1]}
+                longitude={selectedPark.geometry.coordinates[0]}
+                onClose={() => {
+                  setSelectedPark(null);
+                }}
+              >
+                <ParkingPopup />
+              </Popup>
+            ))
+          : null}
 
-            return handleViewportChange({
-                ...newViewport,
-                ...geocoderDefaultOverrides
-            });
-        },
-        []
-    );
-
-    return (
-        <div   >
-            <div className="map_continaer" >
-                <div>
-                    <img className="logo__tp" src={logo} />
-                    {/* <h2 className="App_title"> We Park</h2> */}
-
-                </div>
-
-                {
-                    navigator.geolocation.getCurrentPosition(function (position) {
-                        console.log("Latitude is :", position.coords.latitude);
-                        console.log("Longitude is :", position.coords.longitude);
-                    })
-                }
-                <MapGL
-
-                    ref={mapRef}
-                    {...viewport}
-                    width="90vw"
-                    height="70vh"
-                
-                    onViewportChange={handleViewportChange}
-                    mapboxApiAccessToken={MAPBOX_TOKEN}
-                    mapStyle='mapbox://styles/mapbox/streets-v11'
-                    points
-                    onClick={(e) => { console.log(e) }}
-                >
-                    {parkDate.features.map(park => (
-
-
-                        <Marker
-                            key={park.properties.PARK_ID}
-                            latitude={park.geometry.coordinates[1]}
-                            longitude={park.geometry.coordinates[0]}
-                        >
-
-                             <button
-                                className="marker-btn button1 "
-                                onClick={e => {
-                                    e.preventDefault();
-                                    setSelectedPark(park);
- 
-                                }}
-                            >
-                                {park.properties.NAME}
-                                <br></br>
-                                {park.properties.PARK_ID}
-                            </button>
-                        </Marker>
-
-
-                    ))}
-
-
-                    {selectedPark ? (
-                        //  alert(selectedPark.properties.NAME),
-                        setTimeout(function () { setSelectedPark(null); }, 3000),
-                        
-                        <Popup
-                            latitude={selectedPark.geometry.coordinates[1]}
-                            longitude={selectedPark.geometry.coordinates[0]}
-                            onClose={() => {
-                                setSelectedPark(null);
-                            }}
-                        >
-                                                                <ParkingPopup />
-
-                            <div>
-                                <h2>{selectedPark.properties.NAME}</h2>
-                                <p>{selectedPark.properties.DESCRIPTIO}</p>
-                            </div>
-                        </Popup>
-                    ) : null
-                    
-                    }
-
-
-
-
-                    <GeolocateControl
-                        positionOptions={{ enableHighAccuracy: true }}
-                        trackUserLocation={true}
-                        showUserLocation={true}
-                    />
-                    <div style={{ position: 'absolute', right: 0 }}>
-                        <NavigationControl />
-                    </div>
-
-                    {getListOfParkingWithDis()}
-                    <mapbox-geolocate-control />
-
-                </MapGL>
-                <Geocoder
-                    mapRef={mapRef}
-                    onViewportChange={handleGeocoderViewportChange}
-                    mapboxApiAccessToken={MAPBOX_TOKEN}
-                    position="bottom-right"
-
-                />
-            </div>
+        <GeolocateControl
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+          showUserLocation={true}
+        />
+        <div style={{ position: "absolute", right: 0 }}>
+          <NavigationControl />
         </div>
-    );
 
+        {getListOfParkingWithDis()}
+        <mapbox-geolocate-control />
+      </MapGL>
+      <Geocoder
+        mapRef={mapRef}
+        onViewportChange={handleGeocoderViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        position="bottom-right"
+      />
+    </div>
+  );
 
+  function getListOfParkingWithDis() {
+    const lat1 = 32.9374355,
+      lon1 = 35.2697695,
+      lat2 = 32.794044,
+      lon2 = 34.989571;
 
-    function getListOfParkingWithDis() {
-        const lat1 = 32.9374355, lon1 = 35.2697695, lat2 = 32.794044, lon2 = 34.989571;
+    const R = 6371e3; // metres
+    const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (lat2 * Math.PI) / 180;
+    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
 
-        const R = 6371e3; // metres
-        const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
-        const φ2 = lat2 * Math.PI / 180;
-        const Δφ = (lat2 - lat1) * Math.PI / 180;
-        const Δλ = (lon2 - lon1) * Math.PI / 180;
+    const a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        const d = R * c/1000; // in metres
-        // d = (d / 1000);
-        console.log("dd", d);
-    }
-
+    const d = (R * c) / 1000; // in metres
+    // d = (d / 1000);
+    console.log("dd", d);
+  }
 };
 
-export default Example
+export default Example;
