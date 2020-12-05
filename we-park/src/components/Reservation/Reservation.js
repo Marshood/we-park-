ca0af5ca5148cdd069676e94938adf3258524a10
Marshood Ayoub <marshood.ayoub@gmail.com>
2020-12-05 04:05:09 +0200

    adding buttons to reservation page

--- we-park/src/components/Reservation/Reservation.js
+++ we-park/src/components/Reservation/Reservation.js
@@ -1,30 +1,41 @@
 import React, { useState } from 'react';
 import ParkingPopUp from "../ParkingPopup/index";
 import * as parkDate from "../Map/data/skateboard-parks.json";
-
-
+import navigate from '../../assets/navigate.svg'
 import ParkingPopup from '../ParkingPopup/index';
-import { Link, useLocation } from "react-router-dom";
-
+import './Reservation.css'
+import {
+    BrowserRouter as Router,
+    useLocation,
+    Link,
+    useHistory,
+} from "react-router-dom";
 const Reservation = (props) => {
     let location = useLocation();
+    let history = useHistory();
 
     const { state } = props;
     const [value, onChange] = useState('10:00');
     console.log("selectedPark", location)
-    return (<div style={{  position: "absolute" 
-    ,textAlign: "center" ,padding:"80px"
- }}>
-
-
-{/* position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center; */}
-
+    return (<div style={{
+        position: "absolute"
+        , textAlign: "center", padding: "80px"
+    }}>
+        
         <ParkingPopup selectedPark={location.state} />
+        <div>
+        <img style={{width:"10vw",margin:"20px",marginTop:"30px"}}  src={navigate}></img>
+        </div>
+         <Link  to={{
+            pathname: "/Timer",
+        }}
+        >
+            <button className="button">  Reserved</button>
+        </Link>
+        <button className="button" onClick={() => {
+            history.push("/Example")
 
+        }}>Back</button>
     </div>
     );
 
