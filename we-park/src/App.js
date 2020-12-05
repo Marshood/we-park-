d680a3ed7a324d27bbcbb2a598fb5a08063fa488
Nivin <nivin.abbas1@gmail.com>
2020-12-05 13:49:50 +0200

    added component TimerUp

--- we-park/src/App.js
+++ we-park/src/App.js
@@ -14,6 +14,7 @@ import Profile from "./components/Profile/Profile";
 import Nav from "./components/NavBar/index";
 import Timer from "./components/Timer/Timer";
 import Reservation from "./components/Reservation/Reservation";
+import TimerUp from "./components/Timer/TimerUp";
 function App() {
   let history = useHistory();
 
@@ -27,6 +28,9 @@ function App() {
           <Switch>
             <Route path="/Timer">
               <Timer />
+            </Route>
+            <Route path="/TimerUp">
+              <TimerUp />
             </Route>
             {/* <Route path="/SideBar">
               <NavBar />
